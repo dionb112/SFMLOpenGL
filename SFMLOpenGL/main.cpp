db770458b4821eb74a30a0f998295d9a1c7a3552
Dion <dion-b@hotmail.com>
2017-12-07 12:11:43 +0000

    Solid Starter kit instructions

---
+++ SFMLOpenGL/main.cpp
@@ -0,0 +1,17 @@
+/// <summary>
+/// @author Dion Buckley
+/// 
+/// NOTES:
+///		GLU is important for openGL
+///		Model view
+///		draw method using older method (immidiate mode)
+///		Notice weird ass rotations
+/// </summary>
+/// 
+#include <Game.h>
+
+int main(void)
+{
+	Game& game = Game();
+	game.run();
+}
